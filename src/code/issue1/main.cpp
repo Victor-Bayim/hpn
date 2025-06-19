9804bbfb4722e76ec565d69cb9a6c5ce45048a6d
moningchen <moningchen@tencent.com>
2025-06-19 17:19:37 +0800

    设置项目框架结构

---
+++ src/code/issue1/main.cpp
@@ -0,0 +1,8 @@
+/*************************************************************************
+ * Copyright (c) 2025, TENCENT CORPORATION. All rights reserved.
+ *
+ * See LICENSE.txt for license information
+ * 
+ * Author: moningchen@tencent.com
+ * Content: Main Function For Issue 1
+ ************************************************************************/
